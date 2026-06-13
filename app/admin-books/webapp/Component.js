13aa693f2812dc647a36ceb72a68da4b623d657a
Raga Manjari Ponnaganti <manjari.ponnaganti@esmt.berlin>
2026-06-13 07:37:17 +0000

    First Commit

---
+++ app/admin-books/webapp/Component.js
@@ -0,0 +1,3 @@
+sap.ui.define(["sap/fe/core/AppComponent"], ac => ac.extend("generator-saphanacademy-cap.admin-books.Component", {
+  metadata:{ manifest:'json' }
+}))
---
+++ app/browse/webapp/Component.js
@@ -0,0 +1,3 @@
+sap.ui.define(["sap/fe/core/AppComponent"], ac => ac.extend("generator-saphanacademy-cap.browse.Component", {
+  metadata:{ manifest:'json' }
+}))
